fdc91bbc75943053e1c8f672a9e58ae45d5ec39d
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-05-06 13:31:44 +0530

    start creating user signup and login authentication system using django built-in 'User' authentication system

--- CRUD_project1/core/urls.py
+++ CRUD_project1/core/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('signup/', views.UserSignUpView.as_view(), name='user_signup'),
+  path('login/', views.UserLoginView.as_view(), name='user_login'),
   path('update/<int:stud_id>/', views.update, name='update'),
   path('confirm-delete/<int:stud_id>/', views.confirm_delete, name='confirm_delete'),
   path('<int:stud_id>/', views.delete, name='delete'),
